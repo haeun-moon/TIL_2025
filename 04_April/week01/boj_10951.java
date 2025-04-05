5d4e233fde662b9671f32d154f824fcddc94ee02
haeun-moon <seenmoon12@gmail.com>
2025-04-05 22:27:16 +0900

    0405 solved boj step3

---
+++ 04_April/week01/boj_10951.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj_10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        String s;
+
+        while ((s = br.readLine()) != null) {
+            st = new StringTokenizer(s);
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            bw.write(A + B + "\n");
+        }
+        bw.close();
+    }
+}
